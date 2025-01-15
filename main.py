b8d940ee28ef9eb27cd5038c5651e856d83f78e5
qwiqw <nuralinovaviktoria@gmail.com>
2025-01-15 22:00:04 +0300

    screen

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hello')
+import pygame
+
+pygame.init()
+
+size = wight, height = 1000, 600
+
+pygame.display.set_caption('Russian fishing')
+screen = pygame.display.set_mode(size)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill('black')
+
+    pygame.display.flip()
+pygame.quit()
